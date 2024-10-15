6c295c5e835dc39bc2c0a91ac12a77728663f1c6
Kyng <kynglawal@gmail.com>
2024-10-15 01:31:45 +0100

    Add Pages and Animations

---
+++ app/home/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const home = () => {
+  return (
+    <div className='h-screen p-5'>
+        <header><Navbar /></header>
+        <main className='mt-5'>
+            Loading...
+        </main>
+    </div>
+  )
+}
+
+export default home
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,39 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Landing() {
+  const router = useRouter();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    // Set a small delay to simulate the image loading after the blank screen
+    const timeout = setTimeout(() => setIsLoaded(true), 500); // 0.5s delay before image appears
+    return () => clearTimeout(timeout); // Cleanup on unmount
+  }, []);
+
+  const handleImageClick = () => {
+    router.push("/home");
+  };
+
   return (
-    <div>LANDING PAGE</div>
+    <div className="h-screen flex justify-center items-center bg-black">
+      <div>
+        <button onClick={handleImageClick}>
+          <Image
+            src="/Sanera.png"
+            alt="OG Sanera"
+            width={250}
+            height={250}
+            priority
+            className={`rounded-full shadow-sm shadow-purple-400 transform ${
+              isLoaded ? "animate-slide-up" : "opacity-0"
+            } image-hover-effect`}  // Add hover effect class
+          />
+        </button>
+      </div>
+    </div>
   );
 }
