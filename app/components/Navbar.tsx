c3e0a2d5ed40f21d420d0f5db2b4789770b5a124
Kyng <kynglawal@gmail.com>
2024-11-05 13:50:41 +0100

    add modals and button functionality

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,41 +1,82 @@
 "use client";
 
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import Modal from "./Modal";
 
 const Navbar = () => {
-  const handleClick = () => {
-    console.log("message");
+  const [isAboutModalVisible, setIsAboutModalVisible] = useState(false);
+  const [isServicesModalVisible, setIsServicesModalVisible] = useState(false);
+  const router = useRouter();
+
+  const handleContactClick = () => {
+    router.push("/home/contact");
+  };
+
+  const handleServicesClick = () => {
+    setIsServicesModalVisible(true);
+    setIsAboutModalVisible(false);
+    console.log(isServicesModalVisible);
+  };
+
+  const handleAboutClick = () => {
+    setIsAboutModalVisible(true);
+    setIsServicesModalVisible(false);
+    console.log(isAboutModalVisible);
+  };
+
+  const handleNameClick = () => {
+    router.push("/");
   };
 
   return (
     <div>
       <div className="navbar bg-indigoPurple rounded-2xl p-5">
         <div className="flex-1">
-          <a className="font-extrabold text-xl">SANERA</a>
+          <p
+            className="font-extrabold text-xl text cursor-pointer text-transparent bg-clip-text bg-gradient-to-r from-pink1 to-amber transition-transform duration-200 ease-linear transform hover:scale-110"
+            onClick={handleNameClick}
+          >
+            SANERA
+          </p>
         </div>
         <div className="flex justify-evenly gap-5 lg:gap-14 text-sm font-bold">
           <div
             className="uppercase relative inline-block cursor-pointer transition-transform duration-500 ease-linear transform hover:scale-110 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-pink1 hover:to-amber"
-            onClick={handleClick}
+            onClick={handleAboutClick}
           >
             About
           </div>
 
           <div
             className="uppercase relative inline-block cursor-pointer transition-transform duration-500 ease-linear transform hover:scale-110 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-pink1 hover:to-amber"
-            onClick={handleClick}
+            onClick={handleServicesClick}
           >
             Services
           </div>
 
           <div
             className="uppercase relative inline-block cursor-pointer transition-transform duration-500 ease-linear transform hover:scale-110 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-pink1 hover:to-amber"
-            onClick={handleClick}
+            onClick={handleContactClick}
           >
             Contact
           </div>
         </div>
       </div>
+      {isAboutModalVisible && (
+        <Modal
+          className="text-customGold text-3xl uppercase border border-customGold rounded-box p-6 flex flex-col text-center items-center bg-base-300 bg-opacity-60"
+          content={<div className="flex flex-col items-center">Loading...</div>}
+          onClose={() => setIsAboutModalVisible(false)}
+        />
+      )}
+      {isServicesModalVisible && (
+        <Modal
+          className="text-customGold text-3xl uppercase border border-customGold rounded-box p-6 flex flex-col text-center items-center bg-base-300 bg-opacity-60"
+          content={<div className="flex flex-col items-center">Loading...</div>}
+          onClose={() => setIsServicesModalVisible(false)}
+        />
+      )}
     </div>
   );
 };
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,32 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Navbar from "../components/Navbar";
-
-const handleClick = () => {
-  console.log("message");
-};
+import Modal from "../components/Modal";
 
 const home = () => {
+  const [isAboutModalVisible, setIsAboutModalVisible] = useState(false);
+  const [isServicesModalVisible, setIsServicesModalVisible] = useState(false);
+  const router = useRouter();
+
+  const handleContactClick = () => {
+    router.push("/home/contact");
+  };
+
+  const handleServicesClick = () => {
+    setIsServicesModalVisible(true);
+    setIsAboutModalVisible(false);
+    console.log(isServicesModalVisible);
+  };
+
+  const handleAboutClick = () => {
+    setIsAboutModalVisible(true);
+    setIsServicesModalVisible(false);
+    console.log(isAboutModalVisible);
+  };
+
   return (
     <div className="h-full p-5">
       <header>
@@ -17,28 +35,30 @@ const home = () => {
       <main className="flex flex-wrap mt-5">
         <div className="flex lg:flex-1 flex-wrap justify-center mr-5">
           <div className="flex flex-row p-2 mb-2 rounded-box w-full">
-            <div className="card bg-weirdWhite text-primary-content flex flex-grow mr-5">
+            <div className="card bg-amber text-primary-content flex flex-grow mr-5">
               <div className="card-body">
-                <h2 className="card-title">About Me</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Rerum provident ea adipisci vel exercitationem dicta, ullam
-                  laboriosam, sint, magnam eaque sequi facilis pariatur tenetur
-                  voluptatum quibusdam! Placeat, consectetur. Recusandae,
-                  laborum!
+                <h2 className="card-title font-extrabold text-4xl">About Me</h2>
+                <p className="font-semibold">
+                  Hi, I'm Sanera. I'm a web3 enthusiast and extraordinaire.
+                  Having spent a considerable amount of time honing my craft and
+                  skills, and achieving standard results to prove my competence
+                  and excellence, I'm confident I can produce excellent results
+                  in whatever you might want me to do for you.
                 </p>
-              </div>
-              <div className="card-body">
-                <h2 className="card-title text-sm text-pink1">
+
+                <h2 className="card-title text-xs text-amber mt-4 text-transparent bg-clip-text bg-gradient-to-r from-pink1 to-white">
                   What else do I have to say?
                 </h2>
-                <p className="font-bold">
-                  The path to <span className="text-pink1">enlightenment</span>{" "}
-                  is paved with the footsteps of those who dared to try
+                <p className="font-bold text-xs leading-relaxed">
+                  "Don't be afraid to take that{" "}
+                  <span className="text-pink1">leap, </span>
+                  Just do whatever it is you've always wanted. You're actually
+                  running out of time so stop wasting it... <br />
+                  GM 🤍"
                 </p>
               </div>
             </div>
-            <div className="card lg:w-56 w-48 min-w-48 relative overflow-hidden">
+            <div className="card lg:w-56 w-48 min-w-48 relative overflow-hidden border-amber border-y-2">
               <div className="card-body p-0 relative h-full">
                 {/* Image covers the entire card */}
                 <Image
@@ -51,6 +71,7 @@ const home = () => {
                 />
 
                 {/* Button positioned on top of the image */}
+                <div className="bg-transparent absolute top-0 min-w-48 h-2 z-10 shadow-md shadow-black"></div>
                 <div className="absolute top-2 right-2 z-10">
                   <button className="btn z-20 bg-transparent border-none">
                     <Image
@@ -66,16 +87,21 @@ const home = () => {
           </div>
 
           <div className="flex flex-row p-2 mb-5 rounded-box w-full">
-            <div className="card bg-weirdWhite text-primary-content mr-5 flex flex-1">
+            <div className="card bg-amber text-primary-content mr-5 flex flex-1">
               <div className="card-body">
-                <div className="justify-items-center mb-10">
-                  <h2 className="card-title uppercase">Portfolio</h2>
+                <div className="justify-items-center mb-10 font-extrabold">
+                  <h2 className="card-title uppercase font-extrabold text-2xl">
+                    Portfolio
+                  </h2>
                   <p className="font-extrabold">&</p>
                   <p className="text-pink1">resume</p>
                 </div>
                 <div className="card-actions justify-center">
                   <div className="btn bg-indigoPurple border-none">
-                    <button className="btn bg-indigoPurple border-none text-white uppercase font-bold relative inline-block cursor-pointer transition-transform duration-2000 ease-linear transform hover:scale-110 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-pink1 hover:to-amber">
+                    <button
+                      className="btn bg-indigoPurple border-none text-white uppercase font-bold relative inline-block cursor-pointer transition-transform duration-200 ease-linear transform hover:scale-110 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-pink1 hover:to-amber"
+                      onClick={handleAboutClick}
+                    >
                       View
                     </button>
                   </div>
@@ -84,8 +110,8 @@ const home = () => {
             </div>
 
             <div
-              className="card bg-electricPurple relative text-primary-content flex flex-1 transition-transform duration-300 transform hover:scale-110 hover:cursor-pointer"
-              onClick={handleClick}
+              className="card bg-electricPurple relative text-primary-content flex flex-1 transition-transform duration-300 transform hover:scale-110 hover:cursor-pointer hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-pink1 hover:to-amber hover:border-electricPurple"
+              onClick={handleContactClick}
             >
               <div className="card-body">
                 <h2 className="card-title font-extrabold text-4xl">CONTACT</h2>
@@ -103,29 +129,29 @@ const home = () => {
         </div>
 
         <div className="flex flex-col justify-center items-center w-full lg:w-auto rounded-box p-5">
-          <div className="card bg-weirdWhite text-primary-content w-full lg:w-96 mb-5">
+          <div className="card bg-amber text-primary-content w-full lg:w-96 mb-5">
             <div className="card-body items-center">
-              <h2 className="card-title mb-10">
+              <h2 className="card-title mb-10 font-bold text-2xl">
                 SERVICE<span className="text-electricPurple">info</span>
               </h2>
               <div className="flex flex-col text-white text-2xl font-bold items-center">
-                <div className="btn bg-indigoPurple border-none mb-5">
-                  <button className="btn bg-indigoPurple border-none text-2xl text-white uppercase font-bold relative inline-block cursor-pointer transition-transform duration-2000 ease-linear transform hover:scale-110 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-pink1 hover:to-amber">
+                <div className="btn bg-indigoPurple border-none mb-10">
+                  <button className="btn bg-indigoPurple border-none text-2xl text-white uppercase font-bold relative inline-block cursor-pointer transition-transform duration-200 ease-linear transform hover:scale-110 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-pink1 hover:to-amber">
                     TWEETS
                   </button>
                 </div>
-                <div className="btn bg-indigoPurple border-none mb-5">
-                  <button className="btn bg-indigoPurple border-none text-2xl text-white uppercase font-bold relative inline-block cursor-pointer transition-transform duration-2000 ease-linear transform hover:scale-110 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-pink1 hover:to-amber">
+                <div className="btn bg-indigoPurple border-none mb-10">
+                  <button className="btn bg-indigoPurple border-none text-2xl text-white uppercase font-bold relative inline-block cursor-pointer transition-transform duration-200 ease-linear transform hover:scale-110 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-pink1 hover:to-amber">
                     THREADS
                   </button>
                 </div>
-                <div className="btn bg-indigoPurple border-none mb-5">
-                  <button className="btn bg-indigoPurple border-none text-2xl text-white uppercase font-bold relative inline-block cursor-pointer transition-transform duration-2000 ease-linear transform hover:scale-110 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-pink1 hover:to-amber">
+                <div className="btn bg-indigoPurple border-none mb-10">
+                  <button className="btn bg-indigoPurple border-none text-2xl text-white uppercase font-bold relative inline-block cursor-pointer transition-transform duration-200 ease-linear transform hover:scale-110 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-pink1 hover:to-amber">
                     SPACES & AMAs
                   </button>
                 </div>
-                <div className="btn bg-indigoPurple border-none mb-5">
-                  <button className="btn bg-indigoPurple border-none text-2xl text-white uppercase font-bold relative inline-block cursor-pointer transition-transform duration-2000 ease-linear transform hover:scale-110 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-pink1 hover:to-amber">
+                <div className="btn bg-indigoPurple border-none mb-10">
+                  <button className="btn bg-indigoPurple border-none text-2xl text-white uppercase font-bold relative inline-block cursor-pointer transition-transform duration-200 ease-linear transform hover:scale-110 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-pink1 hover:to-amber">
                     SPECIAL PACKAGE
                   </button>
                 </div>
@@ -164,6 +190,20 @@ const home = () => {
           </div>
         </div>
       </main>
+      {isAboutModalVisible && (
+        <Modal
+          className="text-customGold text-3xl uppercase border border-customGold rounded-box p-6 flex flex-col text-center items-center bg-base-300 bg-opacity-60"
+          content={<div className="flex flex-col items-center">Loading...</div>}
+          onClose={() => setIsAboutModalVisible(false)}
+        />
+      )}
+      {isServicesModalVisible && (
+        <Modal
+          className="text-customGold text-3xl uppercase border border-customGold rounded-box p-6 flex flex-col text-center items-center bg-base-300 bg-opacity-60"
+          content={<div className="flex flex-col items-center">Loading...</div>}
+          onClose={() => setIsServicesModalVisible(false)}
+        />
+      )}
     </div>
   );
 };
