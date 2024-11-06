a43ce379e4b8da124de0235d5d637118d0dbc6e5
Kyng <kynglawal@gmail.com>
2024-11-06 01:53:09 +0100

    add flicker and tracking-in-expand from animista animation

--- app/page.tsx
+++ app/page.tsx
@@ -20,19 +20,22 @@ export default function Landing() {
 
   return (
     <div className="h-screen flex justify-center items-center bg-black">
-      <div>
-        <button onClick={handleImageClick}>
+      <div className="flex flex-col lg:flex-row items-center gap-5 p-5 lg:p-0" onClick={handleImageClick}>
+        <button>
           <Image
             src="/Sanera.png"
             alt="OG Sanera"
             width={250}
             height={250}
             priority
-            className={`rounded-full shadow-sm shadow-purple-400 transition-transform duration-300 ${
-              isLoaded ? "animate-slide-up" : "opacity-0"
+            className={`lg:p-10 rounded-full transition-transform duration-300 ${
+              isLoaded ? "flicker-in-1" : "opacity-0"
             } image-hover-effect`}  // Add hover effect class
           />
         </button>
+        <p className={`text-9xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r via-pink1 from-electricPurple to-amber ${
+              isLoaded ? "tracking-in-expand" : "opacity-0"
+            } image-hover-effect`}>SANERA</p>
       </div>
     </div>
   );
